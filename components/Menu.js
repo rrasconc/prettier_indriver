44d7ba3fa6a080364d05b2fdda711c290d795132
Rigoberto Rascon <RRASCON@Grupoalta.local>
2021-11-18 16:28:53 -0700

    menu

--- components/Menu.js
+++ components/Menu.js
@@ -1,15 +1,40 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ScrollView,
+  Dimensions,
+} from "react-native";
+const common = require("../assets/Styles");
 
-const Menu = () => {
-  return <View style={styles.container}></View>;
+const Menu = (props) => {
+  return (
+    <View style={styles.container}>
+      <View style={[styles.banner, { backgroundColor: common.primary.color }]}>
+        <Image
+          source={{
+            uri: "https://play-lh.googleusercontent.com/Cj7enUX5HYiljGLfCTJqY0X4Noq_P-9vdTUKDVWIbjJUWqkac07qUnv6KTb4HF0cbjLw",
+          }}
+          style={{ height: 30, width: 30 }}
+        />
+      </View>
+      {props.children}
+    </View>
+  );
 };
 
 export default Menu;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "white",
-    height: 500,
+    backgroundColor: "#f8f8fa",
+    flex: 3,
+  },
+  banner: {
+    borderTopRightRadius: 15,
+    borderTopLeftRadius: 15,
+    alignItems: "center",
   },
 });
--- screens/Home.js
+++ screens/Home.js
@@ -1,26 +1,48 @@
 import * as React from "react";
 import MapView from "react-native-maps";
-import { StyleSheet, Text, View, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  ScrollView,
+  FlatList,
+} from "react-native";
 import Menu from "../components/Menu";
+import Card from "../components/Card";
+
+const common = require("../assets/Styles");
+const { width } = Dimensions.get("window");
+
+const options = [
+  { option: "Viaje", icon: "https://i.ibb.co/5Gj3Gxm/car.png" },
+  { option: "Confort", icon: "https://i.ibb.co/qmXgGk3/vip.png" },
+  { option: "Flete", icon: "https://i.ibb.co/YNN8mZh/truck.png" },
+  { option: "Entregas", icon: "https://i.ibb.co/fCX27fW/bag.png" },
+];
 
 export default function Home() {
   return (
-    <View style={styles.container}>
+    <>
       <MapView style={styles.map} />
-      <Menu></Menu>
-    </View>
+      <Menu>
+        <FlatList
+          data={options}
+          keyExtractor={(item) => item.option}
+          renderItem={({ item }) => (
+            <Card title={item.option} icon={item.icon} />
+          )}
+          style={styles.options}
+          horizontal={true}
+        />
+      </Menu>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
   map: {
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
+    flex: 2,
   },
+  options: { paddingVertical: 10 },
 });
